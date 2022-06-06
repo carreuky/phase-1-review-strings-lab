6ba491c867916d95bbd1870c57503975b3d2c552
carreuky <carreuky@gmail.com>
2022-06-06 18:35:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser='Grace Hopper';
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const firstInitial = currentUser.charAt(0);
+const restOfTheName = currentUser.slice(1);
+let shortGreeting = `${firstInitial} Welcome, !`
